48eaf6107f4e7e66394968e8cbf5c646ad5ca0f5
rednithin <reddy.nithinpg@live.com>
2021-09-09 11:03:42 +0530

    Fix preflight

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
+import axios from 'axios'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(async () => {
+    let response = await axios.get("http://dev-bt-backend-lb-645605424.ap-southeast-1.elb.amazonaws.com/api/types/");
+    let data = response.data;
+    console.log({ data })
+  }, [])
   return <Component {...pageProps} />
 }
 export default MyApp
